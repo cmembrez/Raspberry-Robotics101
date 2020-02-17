be7f7c736442d923408a1e36a23f516805ae87fb
ET Tech <55145361+ET-Technologies@users.noreply.github.com>
2020-02-17 16:30:30 +0100

    Update picture.py

--- projects/DC-Box/program/picture.py
+++ projects/DC-Box/program/picture.py
@@ -3,6 +3,10 @@ import picamera
 from picamera.array import PiRGBArray
 import time
 
+def settings():
+    global font
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
 def preview_image(path):
     camera = picamera.PiCamera()
     rawCapture = PiRGBArray(camera)
@@ -12,9 +16,8 @@ def preview_image(path):
     imagecv = rawCapture.array
     camera.close()
     imagecv = cv2.rotate(imagecv, cv2.ROTATE_180)
-    imagecv = cv2.resize(imagecv, (0, 0), fx=0.3, fy=0.3)
+    imagecv = cv2.resize(imagecv, (250, 160))
     cv2.imwrite(path, imagecv)
-    cv2.imshow('image', imagecv)
 
 def sidex(path):
     camera = picamera.PiCamera()
@@ -28,4 +31,32 @@ def sidex(path):
     imagecv = cv2.resize(imagecv, (0, 0), fx=0.5, fy=0.5)
     camera.close()
     cv2.imwrite(path, imagecv)
-    cv2.imshow('image', imagecv)
+    
+def upload(image_upload, path):
+    image_resize= cv2.imread (image_upload)
+    image_resize = cv2.resize(image_resize, (250, 160))
+    cv2.imwrite(path, image_resize)
+    
+def canny(image_upload, path_canny):
+    image_resize= cv2.imread (image_upload)
+    image_resize = cv2.resize(image_resize, (150, 100))  
+    image_resize = cv2.Canny(image_resize, 50, 150)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image_resize, "Canny", (10, 90), font, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.imwrite(path_canny,image_resize)
+    
+def gray(image_upload, path_gray):
+    image_resize= cv2.imread (image_upload)
+    image_resize = cv2.resize(image_resize, (150, 100))
+    image_resize = cv2.cvtColor(image_resize, cv2.COLOR_BGR2GRAY)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image_resize, "Gray", (10, 90), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.imwrite(path_gray,image_resize)
+    
+def background(image_upload, path_background):
+    image_resize= cv2.imread (image_upload)
+    image_resize = cv2.resize(image_resize, (150, 100))
+    image_resize = cv2.GaussianBlur(image_resize, (5,5),0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image_resize, "GaussianBlur", (10, 90), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.imwrite(path_background,image_resize)
